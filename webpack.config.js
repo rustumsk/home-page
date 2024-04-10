93bdf90d6f6096462bf22ba626072c5bb3155b78
Rustum <rustumarellano@gmail.com>
2024-04-10 21:53:51 +0800

    added the necessary files

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+ module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Output Management',
+          template: './src/index.html',
+          minify: false,
+        }),
+      ],
+   entry: './src/index.js',
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true
+   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader','sass-loader',],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+ };
